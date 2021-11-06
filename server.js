09426ca2480bf73553049d0c73f34bbc4b1299f2
subin.w <subin@magicboxasia.com>
2021-11-06 15:07:58 +0700

    add hello

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const http = require('http').Server(app)
 const PORT = process.env.PORT || 6666
 
 app.get('/', (req, res) => res.send('hello devilmuk'))
 
 app.post('/webhook', (req, res) => res.sendStatus(200))
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+http.listen(PORT, function(){
+  console.log('listening on', http.address().port)
+})
