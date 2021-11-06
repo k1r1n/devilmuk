0e85cd292fb987c90063d9349756ef4986a86bb7
subin.w <subin@magicboxasia.com>
2021-11-06 15:52:18 +0700

    add reply bot"

--- server.js
+++ server.js
@@ -13,9 +13,26 @@ app.get('/', (req, res) => res.send('hello devilmuk'))
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
+    getUserProfiles()
     reply(reply_token, msg)
     res.sendStatus(200)
 })
+
+
+function getUserProfiles() {
+  var url = 'https://api.line.me/v2/profile'
+  var headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer {8pkfXWW/9CP4icNaZdhCaCUWj9jXTFgb3XAJRV4131qTcztWWZ/uKdZsVUYnZAY2Ej1iXixNwZNHAGwcaBB5nh1xLBpwbttUH0vZZ9C3moPtOADSsLuqtF7fEdyUW7mC6KIeqT1Dhbos7A1dTF61GgdB04t89/1O/w1cDnyilFU=}'
+  };
+  
+  request.get({
+    url,
+    headers,
+  }, (err, res, body) => {
+      console.log('body', JSON.parse(res))
+  })
+}
 
 function reply(reply_token, msg) {
   console.log('msg', msg)
